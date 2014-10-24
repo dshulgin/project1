ec81cf393b333efb9936433b43b472e115f32549
dshulgin <dimashulgin2012@gmail.com>
2014-10-24 22:44:19 +0300

    -

--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ shinyUI(fluidPage(
   titlePanel("title panel"),
   
   sidebarLayout(
-    sidebarPanel(selectInput("Area", "Choose a dataset:",choices = c("rock", "pressure", "cars"))),
+    sidebarPanel(selectInput("Area", "Choose a needed area:",choices = c("rock", "pressure", "cars"))),
       
     mainPanel(plotOutput("plot1"))
   )
