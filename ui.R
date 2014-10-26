00500c66c4646af1b5333a77fca9aa5c00d91b0c
dshulgin <dimashulgin2012@gmail.com>
2014-10-26 03:06:36 +0300

    +

--- ui.R
+++ ui.R
@@ -4,9 +4,23 @@ library (shiny)
 shinyUI(fluidPage(
   titlePanel("Dynamics of morbidity Belarus (carcinoma, 1989 - 2005)"),
   
+  
   sidebarLayout(
-    sidebarPanel(selectInput("Area", "Choose a needed district:",choices = clusters$name, selected = "Минск")),
+    sidebarPanel(
+      selectInput("Area", "Choose a needed district:",choices = clusters$name, selected = "Минск"),
+      br(),
+      br(),
+      br(),
+      img(src = "cat2.png"),
+      br(),
+      br(),
+      br()
+     
+         
+                 ),
+  
       
-    mainPanel(plotOutput("plot1"))
+    mainPanel(plotOutput("plot1")),
+     
   )
 ))
